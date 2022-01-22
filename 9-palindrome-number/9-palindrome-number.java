b6463f8cdfe674618dbcf2c986f0481b0ce091d0
noble-8 <akash7nair@gmail.com>
2022-01-23 02:29:52 +0530

    Time: 13 ms (47.46%), Space: 41.6 MB (22.26%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,16 +1,12 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0)
-            return false;
-        String str= x+"";
-        int r = str.length()-1;
-        int l =0;
-        while(l<r){
-            if(str.charAt(l)!=str.charAt(r))
-                return false;
-            l++;
-            r--;
+        if(x<0){return false;}
+        int y=0;
+        int z = x;
+        while(z>0){
+            y = y*10 + z % 10 ;
+            z = z /10;
         }
-        return true;
+        return x==y;
     }
 }
